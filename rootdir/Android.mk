45d77b53fe3224f7d820a225b5858e30562d6426
Frasczhe97 <anprasetya97@gmail.com>
2021-10-25 03:22:27 +0700

    aqua: Add usb Config from stock
    Signed-off-by: Frasczhe97 <anprasetya97@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -47,3 +47,11 @@ LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := bin/set_zram.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.mt6765.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.mt6765.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
